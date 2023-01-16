3917a17bab9a0a260ab7e94a6a21a2c6f293393a
OngJinHui <jh.jinhui.ong@gmail.com>
2023-01-16 15:24:49 +0800

    5th commit

--- src/CountNearby.js
+++ src/CountNearby.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const CountNearby = (props) => {
 
-    console.log(props.latLon);
+    // console.log(props.latLon);
 
     const [closeToUserPoints, setCloseToUserPoints] = useState([]);
     
@@ -11,16 +11,27 @@ const CountNearby = (props) => {
 
         console.log(latitude, longitude);
 
-        for (let individual_point in props.latLon) {
+        props.latLon.forEach(individual_point => {
             if ((Math.abs(individual_point.Latitude - latitude) < 0.01) && 
-                (Math.abs(individual_point.Longitude - longitude) < 0.01)) {
-                    const placeholderPoint = {...closeToUserPoints, 
-                            nearbyLatitude: individual_point.Latitude, 
-                            nearbyLongitude: individual_point.Longitude};
-                    setCloseToUserPoints(placeholderPoint);
-                    console.log(closeToUserPoints);
+            (Math.abs(individual_point.Longitude - longitude) < 0.01)) {
+                const placeholderPoint = {...closeToUserPoints, 
+                        nearbyLatitude: individual_point.Latitude, 
+                        nearbyLongitude: individual_point.Longitude};
+                setCloseToUserPoints(placeholderPoint);
             }
-        }
+        });
+
+        // for (let individual_point in props.latLon) {
+        //     console.log(individual_point.Latitude - latitude, individual_point.Longitude - longitude);
+        //     if ((Math.abs(individual_point.Latitude - latitude) < 0.01) && 
+        //         (Math.abs(individual_point.Longitude - longitude) < 0.01)) {
+        //             const placeholderPoint = {...closeToUserPoints, 
+        //                     nearbyLatitude: individual_point.Latitude, 
+        //                     nearbyLongitude: individual_point.Longitude};
+        //             setCloseToUserPoints(placeholderPoint);
+        //             console.log(closeToUserPoints);
+        //     }
+        // }
 
     };
 
